9ab9bb5926ad37868ee5b18beb53806d217c7352
Dell <gabiwoutersm@gmail.com>
2026-02-06 15:07:55 -0300

    Implementação - Adicionado código gerador de chave - Adicionado código que vincula a key com o dado URL

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify, redirect
+import random
+import string
 app = Flask(__name__)
+
+urls = {}
+
+def generate_key(length = 6):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+@app.route('/shorten', methods=['POST'])
+def shorten():
+    data = request.get_json()
+    original_url = data['url']
+    
+    key = generate_key()
+    urls['key'] = original_url
+
 
 @app.route('/')
 def hello_word():
