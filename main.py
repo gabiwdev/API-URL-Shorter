58cfcc608162f600adb77d6a0393ae6bf40deca8
Dell <gabiwoutersm@gmail.com>
2026-02-06 11:02:59 -0300

    API - API Criada

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello_word():
+    return 'Hello, Word!'
+
+@app.route('/HTML')
+def troll():
+    return 'Caiu mauzão'
+
+if __name__ == "__main__":
+    app.run(debug=True)
